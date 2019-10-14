3e38ce3ed0f89337f4004be9217911baf1e42b51
km <49762262+Constance2019@users.noreply.github.com>
2019-10-14 23:01:32 +0800

    Create utili.h

---
+++ utili.h
@@ -0,0 +1,11 @@
+#ifndef _UTILI_H_
+#define _UTILI_H_
+#include <iostream>
+#include <assert.h>
+#include <windows.h>
+#include <time.h>
+using namespace std;
+
+typedef  unsigned char u_char;
+typedef  unsigned long u_long;
+#endif
